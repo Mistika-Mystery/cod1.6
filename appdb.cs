2fb762b21541bf5ccc633c6777bcb83513e162ca
10210155 <10210155@B206PC09>
2023-10-10 12:24:13 +0300

    Добавьте файлы проекта.

---
+++ appdb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cod1._6
+{
+    internal class appdb
+    {
+        public static TradeEntities db = new TradeEntities();
+        public static User CurrentUser = new User();
+    }
+}
---
+++ katalog.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace cod1._6
+{
+    /// <summary>
+    /// Логика взаимодействия для katalog.xaml
+    /// </summary>
+    public partial class katalog : Page
+    {
+        public katalog()
+        {
+            InitializeComponent();
+        }
+
+        private void tbback_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void btvhod_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var log = appdb.db.User.FirstOrDefault(x=> x.UserLogin == tblog.Text && x.UserPassword == tbpass.Text);
+            if (log != null)
+            {
+                
+                appdb.CurrentUser = log;
+                NavigationService.Navigate(new tabl());
+                
+            }
+
+            else
+            {
+                MessageBox.Show("Такого пользователя \n      не существует\nПопробуйте еще раз\n           ИЛИ\nЗарегестрируйтесь");
+            }
+        }
+    }
+    
+}
